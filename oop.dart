317fa70026d9cb010ff2ce969ce020186216fe33
rakan almuliki <rakanalmoliki44@gamil.com>
2024-09-13 06:14:04 +0300

    first commit

---
+++ oop.dart
@@ -0,0 +1,46 @@
+class Car {
+  String ? name;
+  int ? id;
+
+   Car(
+this.id,this.name
+   );
+  void hac(){
+    print("$id,$name");
+  }
+}
+void main(){
+
+Car ra=Car(1,"rakan");
+ra.name="rakan fisal";
+
+ra.hac();
+
+}
+class R{
+
+String? name;
+R(String name){
+  this.name=name;
+}
+}
+class A{
+  int ?r;
+
+  A(this.r);
+}
+class K{
+  int ?_r;
+  int?_s;
+K({int ra=0,int s=0})
+:_r=ra,
+_s=s;
+ }
+    
+  
+ 
+
+
+
+
+
